d2d02e8521e5098ec57e3c7659eccd46ca95895f
Frederico Bajouco <frederico.bajouco@gmail.com>
2025-01-17 19:24:58 +0000

    Add user details component with enhanced layout and styling; include SweetAlert2 dependency

--- src/app/details/user-details/user-details.component.ts
+++ src/app/details/user-details/user-details.component.ts
@@ -1,315 +1,365 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-details',
-  imports: [],
-  templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
-})
-export class UserDetailsComponent {
-
-}
-
-
-/*
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { Morada, Utilizadores } from '../../interfaces/utilizadores';
-import { UtilizadoresService } from '../../services/utilizadores.service';
-import { AuthService } from '../../services/auth.service';
+import { Cars, User } from '../../interfaces/user';
+import { UserService } from '../../services/user.service';
+import { AuthenticatorService } from '../../services/authenticator.service';
 import { Router } from '@angular/router';
 import { forkJoin, Observable } from 'rxjs';
-import Swal from 'sweetalert2';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-import moment from 'moment';
-import { UtilizadoresDialogComponent } from '../../forms/utilizadores-edit/utilizadores-edit.component';
+//import { UserDialogComponent } from '../../forms/utilizadores-edit/utilizadores-edit.component';
+import Swal from 'sweetalert2';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ReservasComponent } from "../../components/reservas/reservas.component";
 import { MessageService } from '../../services/message.service';
 import { MatDivider } from '@angular/material/divider';
 
+
 @Component({
-  selector: 'app-utilizadores-details',
-  standalone: true,
-  imports: [
-    FormsModule,
-    CommonModule,
-    RouterModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    ReservasComponent,
-    MatDivider
-  ],
-  templateUrl: './utilizadores-details.component.html',
-  styleUrls: ['./utilizadores-details.component.scss'],
+	selector: 'app-user-details',
+	standalone: true,
+	imports: [
+		FormsModule,
+		CommonModule,
+		RouterModule,
+		ReactiveFormsModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatDivider
+	],
+	templateUrl: './user-details.component.html',
+	styleUrl: './user-details.component.scss'
 })
-export class UtilizadoresDetailsComponent implements OnInit {
-  utilizador: any;
-  morada: Morada[] = [];
-  errorMessage: { [key: string]: any } = {};
 
-  utilizadorForm!: FormGroup;
+export class UserDetailsComponent implements OnInit {
+	// user: any;
+	// cars: Cars[] = [];
 
-  isEditing: boolean = false;
-  isCreating: boolean = false;
-  originalUtilizador: Utilizadores | null = null;
-  email: string = '';
-  setEnd: boolean = false;
+	user: User | null = null;
+	cars: Cars[] = [];
+	errorMessage: { [key: string]: any } = {};
+	
+	userForm!: FormGroup;
+	
+	isEditing: boolean = false;
+	isCreating: boolean = false;
+	originalUser: User | null = null;
+	userID: string = '';
+	setEnd: boolean = false;
+	
+	constructor(
+		private route: ActivatedRoute,
+		private userService: UserService,
+		private router: Router,
+		public authService: AuthenticatorService,
+		private dialog: MatDialog,
+		private messageService: MessageService,
+		private fb: FormBuilder // Aqui foi corrigido, removendo a duplicação
+	) {
+		this.userForm = this.fb.group({
+			userID: [''],
+			name: [''],
+			email: [''],
+			role: [''],
+			phoneNumber: [''],
+			NIF: [''],
+			status: [''],
+			birthDate: [''],
+			driverRating: [''],
+			driverRatingCount: [''],
+			passengerRating: [''],
+			passengerRatingCount: [''],
+			driversLicense: [''],
+			cars: this.fb.array([])  // Definindo o FormArray
+		});
+	}
 
-  constructor(
-    private route: ActivatedRoute,
-    private utilizadoresService: UtilizadoresService,
-    private router: Router,
-    public authService: AuthService,
-    private dialog: MatDialog,
-    private fb: FormBuilder,
-    private messageService: MessageService
-  ) { }
+	loadUserData(): void {
+		this.userService.getUserByUserID(this.userID).subscribe({
+			next: (data) => {
+				this.user = data;
+				this.userForm.patchValue(data);
+			},
+			error: (error) => {
+				console.error('Error fecthing the user:', error);
+			},
+		});
+	}
+
+	ngOnInit(): void {
+		this.userForm = this.fb.group({
+			userID: [''],
+			name: [''],
+			email: [''],
+			role: [''],
+			phoneNumber: [''],
+			NIF: [''],
+			birthDate: [''],
+			status: [''],
+			driverRating: [''],
+			driverRatingCount: [''],
+			passengerRating: [''],
+			passengerRatingCount: [''],
+			driversLicense: [''],
+			cars: this.fb.array([])
+		});
+	
+		// Atribuir o userID corretamente da rota
+		this.route.paramMap.subscribe(params => {
+			this.userID = params.get('userID') || ''; 
+			if (this.userID) {
+				this.loadAllData(this.userID);
+			} else {
+				console.error('userID não encontrado na rota!');
+			}
+		});
+	}
+	
+	// ngOnInit(): void {
+	// 	// const userIDFromToken = this.authenticatorService.getUserEmail();
+
+	// 	// if (emailFromToken) {
+	// 	//   this.email = emailFromToken;
+	// 	//   console.log('EMAIL DO TOKEN:', this.email);
+	// 	// } else {
+	// 	//   console.error('Email não encontrado no token!');
+	// 	//   return;
+	// 	// }
+
+	// 	this.userForm = this.fb.group({
+	// 		userID: [''],
+	// 		name: [''],
+	// 		email: [''],
+	// 		role: [''],
+	// 		phoneNumber: [''],
+	// 		NIF: [''],
+	// 		birthDate: [''],
+	// 		status: [''],
+	// 		driverRating: [''],
+	// 		driverRatingCount: [''],
+	// 		passengerRating: [''],
+	// 		passengerRatingCount: [''],
+	// 		driversLicense: [''],
+	// 		cars: this.fb.group({
+	// 			brand: [''],
+	// 			model: [''],
+	// 			color: [''],
+	// 			licensePlate: [''],
+	// 		}),
 
 
+	// 	});
 
-  loadUtilizadorData(): void {
-    this.utilizadoresService.getUtilizadoresByEmail(this.email).subscribe({
-      next: (data) => {
-        this.utilizador = data;
-        this.utilizadorForm.patchValue(data);
-      },
-      error: (error) => {
-        console.error('Erro ao carregar dados do utilizador:', error);
-      },
-    });
-  }
-  ngOnInit(): void {
-    const emailFromToken = this.authService.getUserEmail();
+	// 	this.userForm.get('userID')?.setValue(this.userID);
 
-    if (emailFromToken) {
-      this.email = emailFromToken;
-      console.log('EMAIL DO TOKEN:', this.email);
-    } else {
-      console.error('Email não encontrado no token!');
-      return;
+	// 	this.loadAllData(this.userID);
+	// }
+
+	loadAllData(userID: string): void {
+		console.log('Chamando API para obter utilizadores com userID:', this.userID);
+
+		if (this.userID) {
+			this.userService.getUserByUserID(this.userID).subscribe({
+				next: (data) => {
+					console.log('Dados carregados:', data);
+					this.user = data;
+					if (this.user) {
+						this.populateUserForm(this.user);
+					}
+				},
+				error: (error) => {
+					console.error('Erro ao carregar dados do utilizador:', error);
+				},
+			});
+		} else {
+			console.error('userID não definido para carregar dados!');
+		}
+	}
+
+	populateUserForm(user: User): void {
+		console.log('Função populateUserForm chamada');
+		if (user) {
+			this.userForm.patchValue({
+				userID: user.userID || '',
+				name: user.name || '',
+				email: user.email || '',
+				role: user.role || '',
+				phoneNumber: user.phoneNumber || '',
+				NIF: user.NIF || '',
+				birthDate: user.birthDate || '',
+				status: user.status === 'active' ? 'active' : 'inactive',
+				driverRating: user.driverRating || '',
+				driverRatingCount: user.driverRatingCount || '',
+				passengerRating: user.passengerRating || '',
+				passengerRatingCount: user.passengerRatingCount || '',
+				driversLicense: user.driversLicense || ''
+			});
+	
+			const carsArray = this.carsFormArray;
+			carsArray.clear(); 
+	
+			if (Array.isArray(user.cars)) {
+				user.cars.forEach((car: Cars) => {
+					carsArray.push(this.fb.group({
+						brand: [car.brand || ''],
+						model: [car.model || ''],
+						color: [car.color || ''],
+						licensePlate: [car.licensePlate || '']
+					}));
+				});
+			} else {
+				console.warn('user.cars não é um array!');
+			}
+
+			
+		}
+	}
+
+    get carsFormArray(): FormArray {
+        return this.userForm.get('cars') as FormArray;
     }
 
-    this.utilizadorForm = this.fb.group({
-      id: [''],
-      nome: [''],
-      email: [''],
-      telefone: [''],
-      NIF: [''],
-      dataNascimento: [''],
-      tipoUtilizador: [''],
-      status: [''],
-      morada: this.fb.group({
-        rua: [''],
-        localidade: [''],
-        codigoPostal: [''],
-        localidadePostal: [''],
-        pais: [''],
-      }),
-    });
+	onSubmit(): void {
+		if (this.userForm.valid) {
+			const formData = this.userForm.value;
+			formData.status = formData.status === 'ativo' ? 'active' : 'inactive';
 
-    this.utilizadorForm.get('email')?.setValue(this.email);
+			console.log('Dados do formulário antes de enviar:', formData);
 
-    this.loadAllData(this.email);
-  }
+			this.userService
+				.updateUserByUserID(formData.userID, formData)
+				.subscribe({
+					next: (response) => {
+						console.log('Utilizador atualizado com sucesso!');
+						Swal.fire(
+							'Sucesso',
+							'O utilizador foi atualizado com sucesso!',
+							'success'
+						);
 
-  loadAllData(email: string): void {
-    console.log('Chamando API para obter utilizadores com email:', this.email);
+						this.userForm.patchValue({
+							status: formData.status,
+						});
 
-    if (this.email) {
-      this.utilizadoresService.getUtilizadoresByEmail(this.email).subscribe({
-        next: (data) => {
-          console.log('Dados carregados:', data);
-          this.utilizador = data;
-          this.populateUtilizadorForm(this.utilizador);
-        },
-        error: (error) => {
-          console.error('Erro ao carregar dados do utilizador:', error);
-        },
-      });
-    } else {
-      console.error('Email não definido para carregar dados!');
-    }
-  }
+						if (this.user) {
+							this.user.status = formData.status === 'active' ? 'active' : 'inactive';
+						}
 
-  populateUtilizadorForm(utilizador: any): void {
-    console.log('Função populateUtilizadorForm chamada');
-    if (utilizador) {
-      this.utilizadorForm.patchValue({
-        id: utilizador.id || '',
-        nome: utilizador.nome || '',
-        email: utilizador.email || '',
-        telefone: utilizador.telefone || '',
-        NIF: utilizador.NIF || '',
-        dataNascimento: utilizador.dataNascimento || '',
-        status: utilizador.status === 'ativo' ? 'ativo' : 'inativo',
-        tipoUtilizador: utilizador.tipoUtilizador || '',
-        morada: {
-          rua: utilizador.morada?.rua || '',
-          localidade: utilizador.morada?.localidade || '',
-          codigoPostal: utilizador.morada?.codigoPostal || '',
-          localidadePostal: utilizador.morada?.localidadePostal || '',
-          pais: utilizador.morada?.pais || '',
-        },
-      });
+						this.loadUserData();
+					},
+					error: (error) => {
+						console.error('Erro ao atualizar utilizador:', error);
+						Swal.fire(
+							'Erro',
+							'Houve um erro ao atualizar o utilizador.',
+							'error'
+						);
+					},
+				});
+		} else {
+			Swal.fire(
+				'Erro',
+				'Por favor, preencha todos os campos obrigatórios.',
+				'error'
+			);
+		}
+	}
 
-      console.log(
-        '### Status do utilizador:',
-        utilizador.status === 'ativo' ? 'Ativo' : 'Inativo'
-      );
-    }
-  }
+	toggleEditMode(): void {
+		this.isEditing = true;
+		this.userForm.patchValue({ ...this.user! });
+	}
 
-  onSubmit(): void {
-    if (this.utilizadorForm.valid) {
-      const formData = this.utilizadorForm.value;
-      formData.status = formData.status === 'ativo' ? 'active' : 'inactive';
+	// editUtilizador(user: User): void {
+	// 	const dialogRef = this.dialog.open(UserDialogComponent, {
+	// 		width: '1200px',
+	// 		data: { utilizador: this.user },
+	// 		autoFocus: true,
+	// 		disableClose: true,
+	// 	});
 
-      console.log('Dados do formulário antes de enviar:', formData);
+	// 	dialogRef.afterClosed().subscribe((result) => {
+	// 		if (result) {
+	// 			const updatedUtilizador: Utilizadores = {
+	// 				...this.utilizador,
+	// 				nome: result.nome ?? this.utilizador.nome,
+	// 				email: result.email ?? this.utilizador.email,
+	// 				telefone: result.telefone ?? this.utilizador.telefone,
+	// 				NIF: result.NIF ? result.NIF.toString() : this.utilizador.NIF,
+	// 				dataNascimento:
+	// 					result.dataNascimento ?? this.utilizador.dataNascimento,
+	// 				status: result.status ?? this.utilizador.status,
+	// 				morada: {
+	// 					rua: result.rua ?? this.utilizador.morada?.rua ?? '',
+	// 					localidade:
+	// 						result.localidade ?? this.utilizador.morada?.localidade ?? '',
+	// 					codigoPostal:
+	// 						result.codigoPostal ?? this.utilizador.morada?.codigoPostal ?? '',
+	// 					localidadePostal:
+	// 						result.localidadePostal ??
+	// 						this.utilizador.morada?.localidadePostal ??
+	// 						'',
+	// 					pais: result.pais ?? this.utilizador.morada?.pais ?? '',
+	// 				},
+	// 			};
 
-      this.utilizadoresService
-        .updateUtilizadoresByEmail(formData.email, formData)
-        .subscribe({
-          next: (response) => {
-            console.log('Utilizador atualizado com sucesso!');
-            Swal.fire(
-              'Sucesso',
-              'O utilizador foi atualizado com sucesso!',
-              'success'
-            );
+	// 			this.utilizadoresService
+	// 				.updateUtilizadoresByEmail(this.utilizador.email, updatedUtilizador)
+	// 				.subscribe({
+	// 					next: () => {
+	// 						console.log('Utilizador atualizado com sucesso! CCC');
+	// 						this.router
+	// 							.navigate(['/utilizadores', updatedUtilizador.email])
+	// 							.then(() => {
+	// 								this.loadUtilizadorData();
+	// 							});
+	// 					},
+	// 					error: (error) => {
+	// 						console.error('Erro ao atualizar utilizador:', error);
+	// 						this.messageService.showSnackbar(error.error.error, 'error');
+	// 					},
+	// 				});
+	// 		}
+	// 	});
+	// }
 
-            this.utilizadorForm.patchValue({
-              status: formData.status,
-            });
+	// getStatusLabel(status: string | null): string {
+	// 	if (!status) return '';
+	// 	switch (status.toLowerCase()) {
+	// 		case 'active':
+	// 		case 'ativo':
+	// 			return 'ativo';
+	// 		case 'inactive':
+	// 		case 'inativo':
+	// 			return 'inativo';
+	// 		default:
+	// 			return status;
+	// 	}
+	// }
 
-            this.utilizador.status =
-              formData.status === 'active' ? 'ativo' : 'inativo';
-
-            this.loadUtilizadorData();
-          },
-          error: (error) => {
-            console.error('Erro ao atualizar utilizador:', error);
-            Swal.fire(
-              'Erro',
-              'Houve um erro ao atualizar o utilizador.',
-              'error'
-            );
-          },
-        });
-    } else {
-      Swal.fire(
-        'Erro',
-        'Por favor, preencha todos os campos obrigatórios.',
-        'error'
-      );
-    }
-  }
-
-  toggleEditMode(): void {
-    this.isEditing = true;
-    this.utilizadorForm.patchValue({ ...this.utilizador! });
-  }
-
-  editUtilizador(utilizador: Utilizadores): void {
-    const dialogRef = this.dialog.open(UtilizadoresDialogComponent, {
-      width: '1200px',
-      data: { utilizador: this.utilizador },
-      autoFocus: true,
-      disableClose: true,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        const updatedUtilizador: Utilizadores = {
-          ...this.utilizador,
-          nome: result.nome ?? this.utilizador.nome,
-          email: result.email ?? this.utilizador.email,
-          telefone: result.telefone ?? this.utilizador.telefone,
-          NIF: result.NIF ? result.NIF.toString() : this.utilizador.NIF,
-          dataNascimento:
-            result.dataNascimento ?? this.utilizador.dataNascimento,
-          status: result.status ?? this.utilizador.status,
-          morada: {
-            rua: result.rua ?? this.utilizador.morada?.rua ?? '',
-            localidade:
-              result.localidade ?? this.utilizador.morada?.localidade ?? '',
-            codigoPostal:
-              result.codigoPostal ?? this.utilizador.morada?.codigoPostal ?? '',
-            localidadePostal:
-              result.localidadePostal ??
-              this.utilizador.morada?.localidadePostal ??
-              '',
-            pais: result.pais ?? this.utilizador.morada?.pais ?? '',
-          },
-        };
-
-        this.utilizadoresService
-          .updateUtilizadoresByEmail(this.utilizador.email, updatedUtilizador)
-          .subscribe({
-            next: () => {
-              console.log('Utilizador atualizado com sucesso! CCC');
-              this.router
-                .navigate(['/utilizadores', updatedUtilizador.email])
-                .then(() => {
-                  this.loadUtilizadorData();
-                });
-            },
-            error: (error) => {
-              console.error('Erro ao atualizar utilizador:', error);
-              this.messageService.showSnackbar(error.error.error, 'error');
-            },
-          });
-      }
-    });
-  }
-
-  getStatusLabel(status: string | null): string {
-    if (!status) return '';
-    switch (status.toLowerCase()) {
-      case 'active':
-      case 'ativo':
-        return 'ativo';
-      case 'inactive':
-      case 'inativo':
-        return 'inativo';
-      default:
-        return status;
-    }
-  }
-
-  desativarConta() {
-    this.messageService.showConfirmationDialog(
-      'Tem certeza que deseja desativar esta conta? (Insira o seu email para validar)',
-      'Desativar conta',
-      this.authService.getUserEmail() || '').subscribe(result => {
-        if (result) {
-          this.utilizadoresService.desativarConta(this.authService.getUserId()).subscribe(
-            () => {
-              this.messageService.showSnackbar('Conta desativada com sucesso!', 'success');
-              this.authService.logout();
-            },
-            (error) => {
-              console.error('Erro ao desativar Conta', error);
-              this.messageService.showSnackbar('Erro ao criar Reserva: ' + error.error.message, 'error');
-            });
-        }
-      });
-    }
+	// desativarConta() {
+	// 	this.messageService.showConfirmationDialog(
+	// 		'Tem certeza que deseja desativar esta conta? (Insira o seu email para validar)',
+	// 		'Desativar conta',
+	// 		this.authService.getUserEmail() || '').subscribe(result => {
+	// 			if (result) {
+	// 				this.userService.desativarConta(this.authService.getUserId()).subscribe(
+	// 					() => {
+	// 						this.messageService.showSnackbar('Conta desativada com sucesso!', 'success');
+	// 						this.authService.logout();
+	// 					},
+	// 					(error) => {
+	// 						console.error('Erro ao desativar Conta', error);
+	// 						this.messageService.showSnackbar('Erro ao criar Reserva: ' + error.error.message, 'error');
+	// 					});
+	// 			}
+	// 		});
+	// 	}
 }
-
-*/

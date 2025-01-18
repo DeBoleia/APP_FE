6ba74cf932c049a6162f5a043d37d0d6786ccb64
ebmarque <ebenezermarquezine@gmail.com>
2025-01-18 19:23:41 +0000

    Add TripComponent and routing for trip management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { TripComponent } from './component/trip/trip.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'trips', component: TripComponent },
 //    { path: 'login', component: LoginComponent }
   ];
   
--- src/app/component/trip/trip.component.ts
+++ src/app/component/trip/trip.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TripsService } from '../../services/trips.service';
+import { Trip } from '../../interfaces/trip';
+
 
 @Component({
   selector: 'app-trip',
-  imports: [],
+  imports: [
+    CommonModule
+
+  ],
   templateUrl: './trip.component.html',
   styleUrl: './trip.component.scss'
 })
-export class TripComponent {
+export class TripComponent implements OnInit{
 
+  trips: Trip[] = [];
+
+  constructor( 
+    private tripsService : TripsService
+  ) { }
+
+  ngOnInit() {
+    this.tripsService.getTrips().subscribe(
+      (data: Trip[]) => {
+        this.trips = data;
+      }
+    );
+    console.log(this.trips);
+  }
 }

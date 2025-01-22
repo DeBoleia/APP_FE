025eaf9fdd74d243b74ac07fa7f3a3f262659dd3
ebmarque <ebenezermarquezine@gmail.com>
2025-01-22 21:14:46 +0000

    Refactor find-trips component to use autocomplete for origin and municipality inputs; update template and styles

--- src/app/component/find-trips/find-trips.component.ts
+++ src/app/component/find-trips/find-trips.component.ts
@@ -11,6 +11,8 @@ import { TripsService } from '../../services/trips.service';
 import { CommonModule } from '@angular/common';
 import { TripDetailComponent } from '../../details/trip-detail/trip-detail.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-find-trips',
   imports: [
@@ -21,15 +23,40 @@ import { MatDialog } from '@angular/material/dialog';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatDivider
+    MatAutocompleteModule,
+    ReactiveFormsModule,
 
   ],
   templateUrl: './find-trips.component.html',
   styleUrl: './find-trips.component.scss'
 })
 export class FindTripsComponent implements OnInit {
+  districts = [
+    "Aveiro",
+    "Beja",
+    "Braga",
+    "Bragança",
+    "Castelo Branco",
+    "Coimbra",
+    "Évora",
+    "Faro",
+    "Guarda",
+    "Leiria",
+    "Lisboa",
+    "Portalegre",
+    "Porto",
+    "Santarém",
+    "Setúbal",
+    "Viana do Castelo",
+    "Vila Real",
+    "Viseu"
+  ]
+
+  municipalities = [];
 
   trips: Trip[] = [];
+
+  originControl: FormControl = new FormControl('');
 
   constructor(
     private tripsService: TripsService,
@@ -37,7 +64,7 @@ export class FindTripsComponent implements OnInit {
   ) { }
 
   tripDetails(tripCode: string) {
-    TripDetailComponent.openDialog(this.dialog, {tripCode: tripCode});
+    TripDetailComponent.openDialog(this.dialog, { tripCode: tripCode });
   }
 
   ngOnInit(): void {

263fe0ce2cba218b93372d9f04c4aa481e7da504
ebmarque <ebenezermarquezine@gmail.com>
2025-01-23 15:35:36 +0000

    Refactor application card component to use Angular Material grid layout; enhance user interface with driver acceptance buttons and improve data loading logic.

--- src/app/component/aplication-card/aplication-card.component.ts
+++ src/app/component/aplication-card/aplication-card.component.ts
@@ -6,12 +6,24 @@ import { MatCardModule } from '@angular/material/card';
 import { UserService } from '../../services/user.service';
 import { User } from '../../interfaces/user';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { StarRatingComponent } from "../star-rating/star-rating.component";
+import { catchError, forkJoin, of } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { TripsService } from '../../services/trips.service';
 
 @Component({
   selector: 'app-aplication-card',
   imports: [
     MatCardModule,
-    CommonModule
+    CommonModule,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule,
+    StarRatingComponent,
+    MatButtonModule
   ],
   templateUrl: './aplication-card.component.html',
   styleUrl: './aplication-card.component.scss'
@@ -22,20 +34,28 @@ export class AplicationCardComponent {
     private authenticationService: AuthenticatorService,
     private messageService: MessageService,
     private applicationService: ApplicationsService,
-    private userService: UserService
-
+    private userService: UserService,
+    private tripsService: TripsService
   ) { }
 
   @Input() applicationCode: string = '';
-  @Input() user: User | undefined;
+  @Input() user: string = '';
   @Input() isDriver: boolean = false;
   application: any;
 
-  ngOnInit(): void {
+  loadData() {
     this.applicationService.getApplicationByApplicationCode(this.applicationCode).subscribe(application => {
       this.application = application;
       console.log('application: ', this.application);
     });
+  }
+   acceptPassenger(userID: string) {
+      this.tripsService.addPassengerToTrip(this.application.trip.tripCode, userID).subscribe (data => {
+        this.loadData();
+      });
+  }
+  ngOnInit(): void {
+    this.loadData();
   }
 }
 
